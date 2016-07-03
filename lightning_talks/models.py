653741d9e099464113ca1e9649a5f578f5080928
Spencer Pearson <suspense@cs.washington.edu>
2016-07-03 10:14:09 -0700

    change LightningTalk.given_on from DateTimeField to DateField
      Dear Princess Celestia,
    
      Today I had my first frustrating experience with Django.
    
      I realized that I had given a database column the wrong type (I made it a datetime instead of a date),
      so I changed its specification in the model, and generated and ran the migration.
    
      However, when I tried to load the LightningTalks page on the admin site, I got an error about
      "non-empty format string passed to object.__format__".
    
      This must have been because the values in the database were copied over without modification,
      so this column (with type `date`) contained values like `2016-01-01 00:00:00`,
      which must have confused Django somehow.
    
      I manually modified the database to strip out the times, because I wasn't in the mood
      to find the proper way of doing it. (Sorry!) This fixed the admin site.
    
      Your Faithful Student,
      Spencer Pearson

--- lightning_talks/models.py
+++ lightning_talks/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class LightningTalk(models.Model):
   title = models.CharField(max_length=255, unique=True)
   speaker = models.CharField(max_length=255)
-  given_on = models.DateTimeField('date given')
+  given_on = models.DateField('date given')
   embed_video_html = models.CharField(max_length=4095)
 
   def __str__(self):

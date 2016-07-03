6dc7ec46e8c35015c51f32aedfcd04a706e5e5ff
Spencer Pearson <suspense@cs.washington.edu>
2016-07-03 00:31:20 -0700

    hook LightningTalks into their index page
      Dear Princess Celestia,
    
      Today I learned more about Django's templating system!
    
      First, recall from a few letters ago that I wrote a placeholder "view" that nominally
      displayed an index of all the lightning talks. I modified that view so that it
      gives the template access to a list of all lightning talks, ordered from most to least recent.
      (adapted from https://docs.djangoproject.com/en/1.9/intro/tutorial03/#write-views-that-actually-do-something )
    
      Then, I adjusted the template to display information about each talk given.
      This involved two discoveries:
    
      - In the template, `{{ my_var | some_filter:args }}` pipes `my_var` through some "filters"
        (see https://docs.djangoproject.com/en/1.9/ref/templates/language/#filters )
        that transform the value and massage it into text.
        I needed to use the `date` filter to display each talk's date the way I wanted.
    
      - Presumably to avoid XSS attacks, Django HTML-escapes some/all values that get inserted into the page using `{{ ... }}`.
        (At the very least, it escapes string values that come from the database.)
        The `safe` filter tells Django not to escape the message.
        I needed this to insert the HTML that will embed each talk's YouTube video.
    
      My pleasure with Django grows with every commit. Maybe that's the alcohol.
    
      Your Faithful Student,
      Spencer Pearson
    
      P.S.: I also learned about the idiom `MyModel.objects.order_by('my_key')`,
            which returns a list of all `MyModel`s from the database, sorted by their `my_key` attributes.

--- lightning_talks/views.py
+++ lightning_talks/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import LightningTalk
+
 # Create your views here.
 
 def index(request):
   # Adapted from https://docs.djangoproject.com/en/1.9/intro/tutorial03/#a-shortcut-render
-  return render(request, 'lightning_talks/index.html', {'all_talks': ['a', 'b', 'c']})
+  return render(request, 'lightning_talks/index.html', {'all_talks': LightningTalk.objects.order_by('-given_on')})

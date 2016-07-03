991944275313eeef0ddc17e01144ba8d8e45754c
Spencer Pearson <suspense@cs.washington.edu>
2016-07-02 23:39:46 -0700

    added model for LightningTalk
      Dear Princess Celestia,
    
      Today I learned how to create models!
    
      A model class represents a database table, and instances of the class represent rows.
      e.g. `LightningTalk.objects.all()` gives a list of all the LightningTalks in the database;
      `my_talk.speaker` retrieves the value of the `speaker` column in the row represented by `my_talk`.
    
      I created a model by pasting some code like
    
        # adapted from https://docs.djangoproject.com/en/1.9/intro/tutorial02/#creating-models
        class LightningTalk(django.db.models.Model):
          title = django.db.models.CharField(max_length=255)
    
      into `lightning_talks/models.py`.
      Evidently, to hook model classes up to the database, Django looks for subclasses of `Model`,
      and looks for attributes of those subclasses that look like they describe database columns.
    
      To update my database, I ran
        python manage.py makemigrations lightning_talks
      which generated a file in `lightning_talks/migrations/`, describing how to create this table; then
        python manage.py migrate
      which actually applied the changes.
    
      Then I created an instance through `LightningTalk(...).save()`,
      run inside a `python manage.py shell` session.
      (as described in https://docs.djangoproject.com/en/1.9/intro/tutorial02/#playing-with-the-api )
    
      Your Faithful Student,
      Spencer Pearson

--- lightning_talks/models.py
+++ lightning_talks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+# adapted from https://docs.djangoproject.com/en/1.9/intro/tutorial02/#creating-models
+class LightningTalk(models.Model):
+  title = models.CharField(max_length=255)
+  speaker = models.CharField(max_length=255)
+  given_on = models.DateTimeField('date given')
+  embed_video_html = models.CharField(max_length=1023)

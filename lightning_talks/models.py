3d3b44537c275f4c0ffecfacd6fdede8eb0eab75
Spencer Pearson <suspense@cs.washington.edu>
2016-07-03 10:20:34 -0700

    add __str__ method to LightningTalk

--- lightning_talks/models.py
+++ lightning_talks/models.py
@@ -8,3 +8,9 @@ class LightningTalk(models.Model):
   speaker = models.CharField(max_length=255)
   given_on = models.DateTimeField('date given')
   embed_video_html = models.CharField(max_length=4095)
+
+  def __str__(self):
+    return '<LightningTalk given by {speaker} on {date:%Y-%m-%d} about {title!r}>'.format(
+      speaker=self.speaker,
+      date=self.given_on,
+      title=self.title)

5e66728e1c7d090d543147b2307ed3c22d9b9f40
Spencer Pearson <suspense@cs.washington.edu>
2016-07-10 18:36:32 -0700

    make Herokuy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn seattlerationality.wsgi --log-file -
--- seattlerationality/settings.py
+++ seattlerationality/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -115,6 +117,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/

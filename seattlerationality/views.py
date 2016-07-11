db5100cae31b756fcf8b38620b743a401421bed6
Spencer Pearson <suspense@cs.washington.edu>
2016-07-10 17:57:59 -0700

    make pretty

--- seattlerationality/views.py
+++ seattlerationality/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 def index(request):
-  return render(request, 'seattlerationality/index.html')
+  return render(request, 'seattlerationality/index.html',
+                {'title': 'Seattle Rationality'})
 
 def calendar(request):
-  return render(request, 'seattlerationality/calendar.html')
+  return render(request, 'seattlerationality/calendar.html',
+                {'title': 'Calendar'})
